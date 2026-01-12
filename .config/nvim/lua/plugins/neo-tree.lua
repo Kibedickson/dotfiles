24dfcdd0976fe77210b86977efe04c9167992136
Dickson <kibedickson3@gmail.com>
2026-01-12 10:06:11 +0300

    feat: neotree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,32 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    close_if_last_window = true,
+    popup_border_style = "rounded",
+    enable_git_status = true,
+    enable_diagnostics = true,
+    default_component_configs = {
+      icon = { folder_closed = "", folder_open = "", folder_empty = "" },
+      name = { trailing_slash = false },
+      git_status = { symbols = { added = "", modified = "", removed = "" } },
+      diagnostics = { symbols = { hint = "", info = "", warning = "", error = "" } },
+    },
     window = {
       position = "float",
     },
     filesystem = {
       filtered_items = {
         visible = true,
+      },
+      follow_current_file = {
+        enabled = true,
+      },
+      use_libuv_file_watcher = true,
+      hijack_netrw_behavior = "open_default",
+    },
+    buffers = {
+      follow_current_file = {
+        enabled = true,
       },
     },
   },
