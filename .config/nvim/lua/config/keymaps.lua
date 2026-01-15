3a8b970b98b481fd286c8fcd5621e8154302a2d5
Dickson <kibedickson3@gmail.com>
2026-01-15 09:32:50 +0300

    feat: opencode

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,22 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Normal mode mappings
-vim.keymap.set("n", "<leader>tt", "<cmd>NeotermToggle<cr>", { desc = "Toggle NeoTerm" })
-vim.keymap.set("n", "<leader>tx", "<cmd>NeotermExit<cr>", { desc = "Exit NeoTerm" })
-
--- Terminal mode mapping to exit
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
--- Normal mode run nearest test
-vim.keymap.set("n", "<leader>tn", function()
-  require("neotest").run.run()
-end, { desc = "Run nearest test" })
--- Normal mode run file
-vim.keymap.set("n", "<leader>tf", function()
-  require("neotest").run.run(vim.fn.expand("%"))
-end, { desc = "Run test file" })
-
 -- Helper function to run a terminal command in a split and close on success
 local function run_php_tool(cmd)
   local current_file = vim.fn.expand("%:p")
