a71f3f383ee20972b1ceecd11dd4ce8d3141453a
dickson-shinrai <dickson.kibe@shinraitechnologies.io>
2026-05-13 09:44:07 +0300

    feat: use opencode go deepseek-v4-pro as default model

--- .config/nvim/lua/plugins/99.lua
+++ .config/nvim/lua/plugins/99.lua
@@ -6,21 +6,12 @@ return {
     local cwd = vim.uv.cwd()
     local basename = vim.fs.basename(cwd)
     _99.setup({
-      model = "github-copilot/gpt-4.1",
+      model = "opencode-go/deepseek-v4-pro",
       logger = {
         level = _99.DEBUG,
         path = "/tmp/" .. basename .. ".99.debug",
         print_on_error = true,
       },
-      --- WARNING: if you change cwd then this is likely broken
-      --- ill likely fix this in a later change
-      ---
-      --- md_files is a list of files to look for and auto add based on the location
-      --- of the originating request.  That means if you are at /foo/bar/baz.lua
-      --- the system will automagically look for:
-      --- /foo/bar/AGENT.md
-      --- /foo/AGENT.md
-      --- assuming that /foo is project root (based on cwd)
       md_files = {
         "AGENT.md",
       },
